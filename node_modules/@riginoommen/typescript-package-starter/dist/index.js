e516665b8840378f28d2b574972b3a5ba3d374b2
Rigin Oommen <riginoommen@gmail.com>
2021-01-23 22:30:57 +0530

    Test

---
+++ node_modules/@riginoommen/typescript-package-starter/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = void 0;
+exports.helloWorld = function () {
+    return 'Howdy!';
+};
+//# sourceMappingURL=index.js.map
